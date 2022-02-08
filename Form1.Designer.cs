20a03c2a60b16ebc6e216d7c19ba4771793774db
Ulyana <83964382+Ulyana999@users.noreply.github.com>
2022-02-08 23:46:09 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace LABA2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.ResultBox = new System.Windows.Forms.TextBox();
+            this.AgeBox = new System.Windows.Forms.TextBox();
+            this.ProfessionBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // NameBox
+            // 
+            this.NameBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.NameBox.Location = new System.Drawing.Point(90, 44);
+            this.NameBox.Multiline = true;
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(359, 49);
+            this.NameBox.TabIndex = 0;
+            // 
+            // ResultBox
+            // 
+            this.ResultBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ResultBox.Location = new System.Drawing.Point(487, 41);
+            this.ResultBox.Multiline = true;
+            this.ResultBox.Name = "ResultBox";
+            this.ResultBox.Size = new System.Drawing.Size(301, 371);
+            this.ResultBox.TabIndex = 3;
+            // 
+            // AgeBox
+            // 
+            this.AgeBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.AgeBox.Location = new System.Drawing.Point(90, 126);
+            this.AgeBox.Multiline = true;
+            this.AgeBox.Name = "AgeBox";
+            this.AgeBox.Size = new System.Drawing.Size(359, 49);
+            this.AgeBox.TabIndex = 4;
+            // 
+            // ProfessionBox
+            // 
+            this.ProfessionBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ProfessionBox.Location = new System.Drawing.Point(90, 209);
+            this.ProfessionBox.Multiline = true;
+            this.ProfessionBox.Name = "ProfessionBox";
+            this.ProfessionBox.Size = new System.Drawing.Size(359, 49);
+            this.ProfessionBox.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.StartButton);
+            this.groupBox1.Controls.Add(this.ProfessionBox);
+            this.groupBox1.Controls.Add(this.AgeBox);
+            this.groupBox1.Controls.Add(this.NameBox);
+            this.groupBox1.Location = new System.Drawing.Point(16, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(455, 377);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Информация";
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(90, 301);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(359, 49);
+            this.StartButton.TabIndex = 6;
+            this.StartButton.Text = "Добавить";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Имя";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Возраст";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(2, 226);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Профессия";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 457);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.ResultBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.TextBox ResultBox;
+        private System.Windows.Forms.TextBox AgeBox;
+        private System.Windows.Forms.TextBox ProfessionBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button StartButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LABA2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            Person new_person;
+
+            if (NameBox.Text == "")
+            {
+                MessageBox.Show("Введите имя");
+            }
+            if (AgeBox.Text != "")
+            {
+                if (ProfessionBox.Text != "")
+                {
+                    new_person = new Person(NameBox.Text, Convert.ToInt32(AgeBox.Text), ProfessionBox.Text);
+                }
+                else
+                {
+                    new_person = new Person(NameBox.Text, Convert.ToInt32(AgeBox.Text));
+                }
+            }
+            else
+            {
+                if (ProfessionBox.Text != "")
+                {
+                    new_person = new Person(NameBox.Text, 0, ProfessionBox.Text);
+                }
+                else
+                {
+                    new_person = new Person(NameBox.Text);
+                }
+            }
+            ResultBox.Text = new_person.GetInformation();
+
+        }
+    }
+}
